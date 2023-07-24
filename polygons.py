2a97d1afaea315ae9ec9aa239dd0b8a48a69a3aa
Aurelia Lim <aureliadotlim@gmail.com>
2023-07-24 15:27:24 +0200

    polygons init

---
+++ polygons.py
@@ -0,0 +1,52 @@
+#Identity whether four sides (given by 4 integers) can form a square, rectangle, or neither
+
+#Input 
+# Each line of input descirbes a single polygon, and contains four space seperated integers S1, S2, S3 and S4
+# Each represents the length of a side of the polygon
+
+#Output
+# a single line which contains 3 space seperated integers, representing the number of squares, rectangles or polygons with 4 sides
+
+#constraints
+#The four integers representing the sides will be such that -2000 <= X <- 2000
+
+#Sample Input
+#36 30 36 30
+#15 15 15 15
+#46 96 90 100
+#86 86 86 86
+#100 200 100 200
+#-100 200 -100 200
+
+#Sample Output
+#2 2 2
+
+#Explanation
+#There are 2 squares, 2 rectangles and 2 other polygons possible with the given set of sides
+
+def shape_finder():
+
+    squares = 0
+    rectangles = 0
+    others = 0
+
+    while True:
+        try:
+            sides = input().split()
+            if len(sides) != 4:
+                break
+            unique_sides = set(sides)
+            if len(unique_sides) == 1:
+                squares += 1
+            elif len(unique_sides) == 2:
+                rectangles += 1
+            else:
+                others += 1
+        except EOFError:
+            break
+
+    print(squares, rectangles, others)
+
+
+shape_finder()
+
--- sum_of_cars.py
+++ sum_of_cars.py
@@ -1,8 +1,8 @@
-#  You are given an List of positions of cars as to where they are parked. You are also given an integer K. 
+#  You are given an List of positions of cars as to where they are parked. You are also given an integer K.
 #  The cars needs to be covered with a roof. You have to find the minimum length of roof that takes to cover K cars.
- 
+
 #  Input : 12,6,5,2      K = 3
- 
+
 #  Explanation :  There are two possible roofs that can cover K cars. One that covers the car in 2,5,6 parking spots and
 #  another roof which covers 5,6,12 parking spots. The length of these two roofs are 5 and 8 respectively. Return 5
 #  since that's the roof with minimum length that covers K cars.
@@ -21,5 +21,5 @@ def min_length_to_cover_roof(cars, k):
     return min_length
 
 
-print(min_length_to_cover_roof([12, 6, 5, 2], 3)) 
+print(min_length_to_cover_roof([12, 6, 5, 2], 4))
 # return 5
