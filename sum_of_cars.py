3dafafe1091435f6c7a9586da72d77a4d2b5a145
Aurelia Lim <aureliadotlim@gmail.com>
2023-07-24 15:12:55 +0200

    add comments

--- sum_of_cars.py
+++ sum_of_cars.py
@@ -11,8 +11,12 @@
 
 def min_length_to_cover_roof(cars, k):
     cars.sort()
+    # set min_length to the length of the entire list of cars
+    # we need to +1 because 5-5 is 0, but we need to cover 1 car
+    # it is inclusive, so we need to add 1
     min_length = cars[-1] - cars[0] + 1
     for i in range(len(cars) - k + 1):
+        # update min_length to the length of the current k cars
         min_length = min(min_length, cars[i + k - 1] - cars[i] + 1)
     return min_length
 
