8eb1dafbf8267d04cbdade31a60ee39dccd75cc8
Aurelia Lim <aureliadotlim@gmail.com>
2023-09-04 10:14:33 +0800

    longest consec

---
+++ leetcode/medium/longest_consecutive_seq.py
@@ -0,0 +1,41 @@
+# Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+# You must write an algorithm that runs in O(n) time.
+# Example 1:
+
+# Input: nums = [100,4,200,1,3,2]
+# Output: 4
+# Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+# Example 2:
+
+# Input: nums = [0,3,7,2,5,8,4,6,0,1]
+# Output: 9
+ 
+# Constraints:
+
+# 0 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+
+
+class Solution:
+    def longestConsecutive(self, nums:[int]) -> int:
+        #if empty, return 0
+        if len(nums) == 0:
+            return 0
+        #remove duplicates & sort
+        nums = list(set(nums))
+        nums.sort()
+        #by default, the longest consecutive sequence is 1
+        chain = 1
+        longest = 1
+    
+        for i in range(len(nums)-1):
+            current = nums[i]
+            if current == nums[i+1] - 1:
+                chain +=1
+            else:
+                chain = 1
+            longest = max(longest, chain)
+
+        return longest
+    
+print(Solution().longestConsecutive([1,2,0,1]))
