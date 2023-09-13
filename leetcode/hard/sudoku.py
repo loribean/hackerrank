8a0a3364e621c2a4962df488cc49d1f21785e2a1
Aurelia Lim <aureliadotlim@gmail.com>
2023-09-13 22:55:01 +0800

    sudoku hard

---
+++ leetcode/hard/sudoku.py
@@ -0,0 +1,77 @@
+# Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+# A sudoku solution must satisfy all of the following rules:
+
+# Each of the digits 1-9 must occur exactly once in each row.
+# Each of the digits 1-9 must occur exactly once in each column.
+# Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+# The '.' character indicates empty cells.
+
+ 
+
+# Example 1:
+
+
+# Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+# Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+# Explanation: The input board is shown above and the only valid solution is shown below:
+
+
+ 
+
+# Constraints:
+
+# board.length == 9
+# board[i].length == 9
+# board[i][j] is a digit or '.'.
+# It is guaranteed that the input board has only one solution.
+
+
+from collections import deque, defaultdict
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        
+        #use dfs to solve the sudoku
+        #use backtracking to solve the sudoku
+        rows,cols,boxes,visited =  defaultdict(set), defaultdict(set), defaultdict(set), deque()
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] != '.':
+                    rows[i].add(board[i][j])
+                    cols[j].add(board[i][j])
+                    boxes[(i//3,j//3)].add(board[i][j])
+                else:
+                    visited.append((i,j))
+
+        def dfs():
+            if not visited:
+                return True
+            i,j = visited[0]
+            box = (i//3,j//3)
+
+            for num in {'1','2','3','4','5','6','7','8','9'}:
+                if num not in rows[i] and num not in cols[j] and num not in boxes[box]:
+                    rows[i].add(num)
+                    cols[j].add(num)
+                    boxes[box].add(num)
+                    board[i][j] = num
+                    visited.popleft()
+                    if dfs():
+                        return True
+                    else:
+                        rows[i].remove(num)
+                        cols[j].remove(num)
+                        boxes[box].remove(num)
+                        board[i][j] = '.'
+                        visited.appendleft((i,j))
+
+            return False
+        
+        dfs()
+
+
+
+        
